28d58f8941e73a2671bdd33f2c69de43cc0bc539
raihanph21 <m.rhn.ph@gmail.com>
2026-01-11 22:05:37 +0700

    chore: add experiment file

--- src/App-v1.js
+++ src/App-v1.js
@@ -36,42 +36,22 @@ export default function App() {
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
 
-          <StepMessage step={step}>
-            {messages[step - 1]}
-            <div className="buttons">
-              <Button bgColor="#e7e7e7" textColor="#333" onClick={() => alert(`Learn How To ${messages[step - 1]}`)}>
-                Learn How
-              </Button>
-            </div>
-          </StepMessage>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+            {/* {test.name} */}
+          </p>
 
           <div className="buttons">
-            <Button textColor="#fff" bgColor="#7950f2" onClick={handlerPrevious}>
-              <span>👈</span> Previous
-            </Button>
-            <Button textColor="#fff" bgColor="#7950f2" onClick={handlerNext}>
-              Next<span>👉</span>
-            </Button>
+            <button style={{ backgroundColor: "#7950f2", color: "#fff" }} onClick={handlerPrevious}>
+              {/* pass the function value not calling the function. klo handlerPrevious() -> calling function */}
+              Previous
+            </button>
+            <button style={{ backgroundColor: "#7950f2", color: "#fff" }} onClick={handlerNext}>
+              Next
+            </button>
           </div>
         </div>
       )}
     </>
-  );
-}
-
-function StepMessage({ step, children }) {
-  return (
-    <div className="message">
-      <h3>Step {step}</h3>
-      {children}
-    </div>
-  );
-}
-
-function Button({ textColor, bgColor, onClick, children }) {
-  return (
-    <button style={{ backgroundColor: bgColor, color: textColor }} onClick={onClick}>
-      {children}
-    </button>
   );
 }
